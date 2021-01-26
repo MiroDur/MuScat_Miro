22660d044912de6ade57cf09154d70537d7b833b
unknown <Miro@MiroBiophotonic.fme.vutbr.cz>
2021-01-26 15:43:29 +0100

    dsf
    fsd

--- MuScatMicroscopeSim.py
+++ MuScatMicroscopeSim.py
@@ -124,20 +124,11 @@ class MuScatMicroscopeSim(tf.keras.Model):
         # filter scattered field by objective pupil
         FiltScatteredField = tf.reshape(
             self.FiltByObjectivePupil(ScatteredField),
-            [-1, 1, 1, self.gridSize[1], self.gridSize[2]])
+            [-1, self.gridSize[1], self.gridSize[2]])
 
         # reference waves are illumination plane waves at z=0
         # and transversally shifted in relation to illumination plane waves
-        referenceWaves = tf.exp(
-            tf.complex(tf.cast(0., tf.float32), 2 * np.pi *
-                       (tf.reshape(self.KtIllum[:, 0], [-1, 1, 1, 1, 1]) *
-                        (tf.reshape(self.realxx, [1, 1, 1, self.gridSize[1],
-                                                  self.gridSize[2]]) -
-                         tf.reshape(refShifts[:, 0], [1, -1, 1, 1, 1])) +
-                        tf.reshape(self.KtIllum[:, 1], [-1, 1, 1, 1, 1]) *
-                        (tf.reshape(self.realyy, [1, 1, 1, self.gridSize[1],
-                                                  self.gridSize[2]]) -
-                         tf.reshape(refShifts[:, 1], [1, -1, 1, 1, 1])))))
+        referenceWaves = self.planeWaves
 
         # apply defocus related to the illumination of volumetric sample
         propagatedIllum = FiltScatteredField * tf.exp(tf.complex(
@@ -162,3 +153,53 @@ class MuScatMicroscopeSim(tf.keras.Model):
                                0) / self.planeWavesNum
 
         return zStack
+
+    def TomoImaging(self, ScatteredField):
+        """
+        Generate images at defined Z positions for each reference obj. shift.
+
+        Indexing order: [illum plane wave, xCoor, yCoor]
+
+        Parameters
+        ----------
+        ScatteredField : Complex64 3D-Tensor
+            Output of light propagation simulation
+            Indexeing order: [illum plane wave, xCoor, yCoor]
+
+        Returns
+        -------
+        zStack : Complex64 4D-Tensor
+            [illum plane wave, xCoor, yCoor]
+
+        """
+        # filter scattered field by objective pupil
+        FiltScatteredField = tf.reshape(
+            self.FiltByObjectivePupil(ScatteredField),
+            [-1, self.gridSize[1], self.gridSize[2]])
+
+        # reference waves are illumination plane waves at z=0
+        # and transversally shifted in relation to illumination plane waves
+        referenceWaves = self.planeWaves
+
+        # apply defocus related to the illumination of volumetric sample
+        propagatedIllum = FiltScatteredField * tf.exp(tf.complex(
+            tf.cast(0., tf.float32),
+            -2 * np.pi * tf.reshape(self.Kzillum, [-1, 1, 1]) *
+            tf.reshape(self.realzzz[0, 0, 0], [-1, 1, 1])))
+
+        # propagate all fields for each illumination plane wave
+        # to z-stack positions
+        propagator = tf.signal.ifftshift(tf.exp(tf.complex(
+            tf.cast(0., tf.float32), 2 * np.pi * self.KzzM * (
+                -((self.gridSize[0]) * self.dz -
+                  tf.reshape(0., [-1, 1, 1]))))), (1, 2))
+
+        propagatedFields = tf.signal.ifft2d(
+            tf.signal.fft2d(propagatedIllum) * propagator)
+
+        # returns summed up interference of scattered fields and
+        # reference waves
+
+        TomoImages = propagatedFields * tf.math.conj(referenceWaves)
+
+        return TomoImages
---
+++ testScriptTomo.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 15:54:25 2020
+
+@author: Miro
+"""
+from MuScatObject import MuScatObject
+from MuScatField import MuScatField
+from MuScatParameters import MuScatParameters
+from MuScatMicroscopeSim import MuScatMicroscopeSim
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow_addons as tfa
+import time
+import numpy as np
+tf.config.set_visible_devices([], 'GPU')
+# %%
+start = time.time()
+parameters = MuScatParameters(lambda0=0.650,         # wavelength in vacuum
+                              gridSize=[50, 32, 32],  # gridSize [z, x ,y]
+                              dx=0.45,          # dx
+                              dy=0.45,          # dy
+                              dz=0.3,           # dz
+                              refrIndexM=1.5,           # refr index in medium
+                              NAc=0.5,           # NAc
+                              NAo=0.5)           # NAo
+centerInd = np.int32(parameters.gridSize[1] / 2)
+regLambdaL1 = 0.0001
+regLambdaL2 = 0.000
+learnRate = 0.01
+refShifts = tf.cast(tf.constant([[0., 0.]]), tf.float32)
+imagedObject = MuScatObject(parameters)
+# imagedObject.GenerateBox(4., 4., 4., 1.52)
+imagedObject.GenerateBead(3., 1.52)
+# imagedObject.GenerateSheppLogan(1.55)
+
+imagingSim = MuScatMicroscopeSim(parameters)
+
+imagingSim.Illumination()
+imagingSim.Detection()
+fieldMeasured = MuScatField(imagingSim.planeWaves, parameters).ComputeMuScat(
+    imagedObject, method='MLB')
+# fieldMeasured.ComputeMuScat(imagedObject, method='MLB')
+zStackMeasured = imagingSim.TomoImaging(fieldMeasured)
+
+print(time.time()-start)
+# %%
+optimizedObject = MuScatObject(parameters)
+regularizer = tf.keras.regularizers.L1L2(regLambdaL1, regLambdaL2)
+# opt = tf.keras.optimizers.Adamax(learning_rate=learnRate)
+opt = tfa.optimizers.Yogi(learning_rate=learnRate,
+                          l1_regularization_strength=regLambdaL1,
+                          l2_regularization_strength=regLambdaL2,)
+
+
+def regTV(variable, parameter):
+    diffZ = (2*variable - tf.roll(variable, 1, 0) - tf.roll(variable, -1, 0))/2
+    diffX = (2*variable - tf.roll(variable, 1, 1) - tf.roll(variable, -1, 1))/2
+    diffY = (2*variable - tf.roll(variable, 1, 2) - tf.roll(variable, -1, 2))/2
+    return parameter * tf.reduce_sum(tf.abs(diffX) + tf.abs(diffY) +
+                                     tf.abs(diffZ))
+
+
+@tf.function
+def loss_fn(zStackSimulated, zStackMeasured):
+    return tf.reduce_mean(tf.abs(zStackMeasured - zStackSimulated)**2) #+\
+        #regularizer(optimizedObject.RIDistrib)
+
+# scatteredField = MuScatField(imagingSim.planeWaves, parameters).ComputeMuScat(optimizedObject, method='MLB')
+
+
+loss = lambda: loss_fn(imagingSim.TomoImaging(
+    MuScatField(imagingSim.planeWaves, parameters).ComputeMuScat(
+        optimizedObject, method='MLB')), zStackMeasured)
+lossF = []
+
+for i in range(10):
+    opt_op = opt.minimize(loss, var_list=[optimizedObject.RIDistrib])
+    plt.figure(1)
+    plt.subplot(121), plt.colorbar(plt.imshow(
+        optimizedObject.RIDistrib[:, centerInd, :],
+        aspect=parameters.dz/parameters.dx))
+    plt.subplot(122), plt.colorbar(plt.imshow(
+        np.angle(zStackMeasured[0, :, centerInd, :]),
+        aspect=parameters.dz/parameters.dx))
+    plt.tight_layout(pad=3.0)
+    plt.show()
+    #lossF.append(loss_fn(imagingSim.CCHMImaging(imagingSim.ComputeScatteredField(optimizedObject), zPositions, refShifts),
+    #                  zStackMeasured).numpy)
+    print(time.time()-start)
